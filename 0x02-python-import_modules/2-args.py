a60effc4697790cb3b97b73392e15e7433512cbf
user14692516 <user14692516>
2023-08-15 01:42:04 -0700

    Correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     import sys
-    args = len(sys.argv)
-    if(args <= 1):
-        print("{} arguments.".format(args - 1))
-    elif(args == 2):
-        print("{} argument:".format(args - 1))
+    argc = len(sys.argv)
+    if (argc == 0):
+        print(argc, arguments.)
+    elif (argc == 1):
+        print(argc, "argument:".format(sys.argv[0]))
+        print("1:", sys.argv[1])
     else:
-        print("{} arguments:".format(args - 1))
-    for z in range(1, args):
-         print("{}: {}".format(z + 1, argv[z + 1]))
+        print(argc, "arguments:")
+        for i in range(1, argc + 1):
+            print("{}: {}".format(i, sys.argv[i]))
