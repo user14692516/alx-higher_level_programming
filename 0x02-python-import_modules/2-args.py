1f904ba6a1f9b5745686cae452bf3c780b8862d9
14692516 <kipkuruidenniskiptoo@gmail.com>
2023-08-16 21:05:57 +0000

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(args - 1))
     for x in range(1, args):
-        print("{}: {}".format(x, sys.argv[x]
+        print("{}: {}".format(x, sys.argv[x]))
